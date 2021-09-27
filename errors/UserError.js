03d375739064e0fb97237e2abb4607e669737943
DeVeom <deveomm@gmail.com>
2021-09-28 05:20:36 +0900

    Modfiy: UserError.js HTTP status code 세분화

--- errors/UserError.js
+++ errors/UserError.js
@@ -2,14 +2,16 @@
 class RegisterFailed extends Error {
   constructor() {
     super();
-
-    if (this instanceof EmailDuplicateError)
-      this.content = "inputted email already exist";
-    if (this instanceof BcryptHashError)
-      this.content = "failed to hash password";
-
-    this.status = 500;
     this.message = "회원가입 실패";
+
+    if (this instanceof EmailDuplicateError) {
+      this.content = "inputted email already exist";
+      this.status = 400;
+    }
+    if (this instanceof BcryptHashError) {
+      this.content = "failed to hash password";
+      this.status = 500;
+    }
   }
 }
 
@@ -20,23 +22,27 @@ class BcryptHashError extends RegisterFailed {}
 class LoginFailed extends Error {
   constructor() {
     super();
+    this.status = 401;
+    this.message = "로그인 실패";
 
     if (this instanceof NonExistentEmailError)
       this.content = "non-existent email";
-    if (this instanceof BcryptCompareError)
-      this.content = "failed to compare passwords";
     if (this instanceof InvaildPasswordError)
       this.content = "invaild password input";
-    if (this instanceof TokenError) this.content = "failed to make a token";
-
-    this.status = 401;
-    this.message = "로그인 실패";
+    if (this instanceof BcryptCompareError) {
+      this.content = "failed to compare passwords";
+      this.status = 500;
+    }
+    if (this instanceof TokenError) {
+      this.content = "failed to make a token";
+      this.status = 500;
+    }
   }
 }
 
 class NonExistentEmailError extends LoginFailed {}
-class BcryptCompareError extends LoginFailed {}
 class InvaildPasswordError extends LoginFailed {}
+class BcryptCompareError extends LoginFailed {}
 class TokenError extends LoginFailed {}
 
 export {
